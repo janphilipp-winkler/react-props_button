f29cf9b5b99d0f4a4425597970c3a935deb8865a
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-15 14:44:49 +0100

    completed challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <>
+      <button onClick={onClick} disabled={disabled} style={{ color: color }}>
+        {text}
+      </button>
+    </>
+  );
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("YOU CLICKED THE BUTTON");
+  }
+  return <Button text="CLICK ME" color="red" onClick={handleClick} />;
 }
